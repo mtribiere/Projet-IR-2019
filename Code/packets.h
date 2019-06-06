a769089f3f9e0f6d7c68a33e2f1ab6011958e8b4
Sonkom <51315496+Sonkom@users.noreply.github.com>
2019-06-06 13:55:17 +0200

    Ajout des sizeofXPacket

--- Code/packets.h
+++ Code/packets.h
@@ -8,27 +8,27 @@ unsigned char connectionStart1[] = {255,
 unsigned char connectionStart2[] = {254,
                                    13,00,00,00};
 
-unsigned char blue[] = {0,
+unsigned char blue[] = {00,
                         98,108,117,101,
                         00};
 
-unsigned char green[] = {0,
+unsigned char green[] = {00,
                         103,114,101,101,110,
                         00};
 
-unsigned char red[] = {0,
+unsigned char red[] = {00,
                        114,101,100,
                        00};
 
-unsigned char cyan[] = {0,
+unsigned char cyan[] = {00,
                         99,121,97,110,
                         00};
 
-unsigned char purple[] = {0,
+unsigned char purple[] = {00,
                          112,117,114,112,108,101,
                          00};
 
-unsigned char yellow[] = {0,
+unsigned char yellow[] = {00,
                          121,101,108,108,111,119,
                          00};
 
