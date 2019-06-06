a158db2614bb177aebf130d3c733a835939c939f
Sonkom <51315496+Sonkom@users.noreply.github.com>
2019-06-06 13:46:46 +0200

    Version packets.h avec toutes les couleurs

--- Code/packets.h
+++ Code/packets.h
@@ -8,10 +8,29 @@ unsigned char connectionStart1[] = {255,
 unsigned char connectionStart2[] = {254,
                                    13,00,00,00};
 
-
 unsigned char blue[] = {0,
-                       98,108,117,101,
+                        98,108,117,101,
+                        00};
+
+unsigned char green[] = {0,
+                        103,114,101,101,110,
+                        00};
+
+unsigned char red[] = {0,
+                       114,101,100,
                        00};
+
+unsigned char cyan[] = {0,
+                        99,121,97,110,
+                        00};
+
+unsigned char purple[] = {0,
+                         112,117,114,112,108,101,
+                         00};
+
+unsigned char yellow[] = {0,
+                         121,101,108,108,111,119,
+                         00};
 
 
 #endif
